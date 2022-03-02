b206ffc77fb58a5a3736d160813f08a75e6360bd
Karim Ibrahim Ramadan <karimibrahimramadan@Karims-MacBook-Air.local>
2022-03-03 01:12:52 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/karim/mynotesapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.karim.mynotesapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/register_view.dart
@@ -0,0 +1,111 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import '../firebase_options.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({Key? key}) : super(key: key);
+
+  @override
+  _RegisterViewState createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _emailController;
+  late final TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          switch (snapshot.connectionState) {
+
+            case ConnectionState.done:
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: 'Enter your email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                  TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                    autocorrect: false,
+                    enableSuggestions: false,
+                    decoration: InputDecoration(
+                      hintText: 'Enter your password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      final email = _emailController.text;
+                      final password = _passwordController.text;
+                      FirebaseAuth _auth = FirebaseAuth.instance;
+                      try {
+                        final userCredential =
+                        await _auth.createUserWithEmailAndPassword(
+                          email: email,
+                          password: password,
+                        );
+                      } on FirebaseException catch (e) {
+                        if (e.code == 'email-already-in-use') {
+                          print('Email is already in use');
+                        } else if (e.code == 'invalid-email') {
+                          print('Invalid email');
+                        } else if (e.code == 'weak-password') {
+                          print('Password must be at least 6 characters');
+                        } else {
+                          print(e.code);
+                        }
+                      }
+                    },
+                    child: const Text(
+                      'Register',
+                    ),
+                  ),
+                ],
+              );
+
+            default:
+              return const Text('Loading...');
+          }
+
+
+        },
+      ),
+    );
+  }
+}
